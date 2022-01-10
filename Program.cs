05c9a9a264919d919423d99e05913dc40d30b330
Denzcel <77713615+Denzcel@users.noreply.github.com>
2022-01-09 16:08:33 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
-namespace Week1Lab1_b
+namespace Week1Lab3_7progFeatures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I confirm I've emailed the instructor any questions/issues I ran into during this lab to get help / resolve them, and have now successfully completed this lab.");
+            // singe line comment
+            /* This is a mult-line
+                 comment 
+            */
+            string address;
+            Console.WriteLine("Enter your address or enter 'None' if no permanent address.");
+            address = Console.ReadLine();
+            if (address.Equals("None"))
+            {
+                Console.WriteLine("Hope you find a nice place to live!");
+            }
+            else
+            {
+                Console.WriteLine("Thanks for entering your address!");
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Repeating this code 3 times");
+            }
+            /* The line below runs the function called “printAge” */
+            printAge(address);
+        }
+        /*The code below goes AFTER the function called “Main” (not inside Main), 
+          because this is a separate function itself. This function takes 1 input parameter 
+          of type string & “void” means it returns no data.
+        */
+        static void printAge(string address)
+        {
+            Console.WriteLine("The address you entered is " + address);
         }
     }
 }
