2931d239b0d38e7504c6c380115229c794993362
Denzcel <77713615+Denzcel@users.noreply.github.com>
2022-01-09 16:05:52 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week1Lab1_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I confirm I've emailed the instructor any questions/issues I ran into during this lab to get help / resolve them, and have now successfully completed this lab.");
+        }
+    }
+}
